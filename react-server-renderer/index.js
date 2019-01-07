c2a07f05eb1b669c826b12bf9f552eb2ac16e99a
evont <evontgoh@foxmail.com>
2019-01-07 17:30:46 +0800

    [refactor] - remove useless files

--- react-server-renderer/index.js
+++
@@ -1,7 +0,0 @@
-var createBundleRendererCreator = require("./lib/bundle-renderer/create-bundle-renderer");
-var createRenderer = require("./lib/create-renderer");
-process.env.REACT_ENV = 'server';
-module.exports = {
-  createRenderer,
-  createBundleRenderer: createBundleRendererCreator(createRenderer),
-}
